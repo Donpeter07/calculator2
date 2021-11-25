9ada450de0f73bb4a42400a93ee6915737d5cd17
don peter <donpeter212@gmail.com>
2021-11-25 21:47:08 +0530

    done

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var displayvalue=(num)=>res.value+=num
+var displayvalue=(num)=>textview.value+=num
 // function displayvalue(num){
 //     let res=document.querySelector("#textview")
 //     res.value+=num
 
 // }
 
-var clearbox=()=>result.value=""
+var clearbox=()=>textview.value=""
 // function clearbox(){
 //     document.querySelector("#textview").value=""
 // }
 
 
-var evaluateexpression=()=>result.value=eval(result.value)
+var evaluateexpression=()=>textview.value=eval(textview.value)
 // function evaluateexpression(){
 //     let res=document.querySelector("#textview")
 //     let cur_val=res.value
